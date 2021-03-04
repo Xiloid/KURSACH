4408d023a6440b1781446e08309f253f7694b236
Xiloid <faustov.roman.a@gmail.com>
2021-03-04 20:20:45 +0200

    commited

--- casino_magic.py
+++ casino_magic.py
@@ -1,58 +1,60 @@
 import random
-import casino_menu
+#from casino_menu import main
+
 
 def magic(all_points):
 #    rules()
     random_number = random.randint(1, 10)
-    print("Вгадайте Магічне число від 1 до 10")
+    print("Угадайте число от 1 до 10")
     counter = 0
-    print("==Ігра починається==\n")
+    print('Игра начинается\n')
     while True:
         try:
             number = int(input("Ваше число: "))
             counter += 1
         except ValueError:
-            print("Це повинно бути число.")
+            print('Должно быть число')
             continue
         if number > random_number:
-            print("Магічне число менше", number)
+            print(f'магическое число меньше {number}')
         elif number < random_number:
-            print("Магічне число більше", number)
+            print(f'магическое число больше {number}')
         else:
-            print("\nВітаю! Ви вгадали число",number,"")
-            print("Витрачено спроб:", counter)
+            print(f'\nВы угадали, число {number}')
+            print("Попыток:", counter)
             random_number = random.randint(1, 10)
             points_1 = 0
             if counter == 1:
-                print(f"Вам начисляно 50 очків")
+                print(f"Вам начислено 50 очкев")
                 points_1 = 50
             elif counter == 2:
-                print("Вам начисляно 25 очків")
+                print("Вам начислено 25 очков")
                 points_1 = 25
             elif counter == 3:
-                print("Вам начисляно 10 очків")
+                print("Вам начислено 10 очков")
                 points_1 = 10
             elif counter == 4:
-                print("Вам начисляно 5 очків")
+                print("Вам начислено 5 очков")
                 points_1 = 5
             else:
-                print("Вам начисляно 0 очків")
+                print("Вам начислено 0 очков")
 
             if all_points == 0:
                 all_points = points_1
             else:
                 all_points += points_1
-            print("Тепер у вас",all_points,"очків")
+            print(f'Теперь у вас {all_points} очков')
             break
 
         if counter == 5:
-            print("\nМагічне число було", random_number)
-            print("Використано забагато спроб")
+            print(f'\nБыло загадано число {random_number}')
+            print('Все попытки кончились')
             break
-    next_action = input("\nСпробувати ще раз?(y/n/menu):")
+    next_action = input('\nИграем ещё раз? (y/n/m - меню): ')
     if next_action == "Y":
         counter = 0
         magic(all_points)
-    elif next_action == "menu":
-        casino_menu()
+    elif next_action == "m":
+        exit()
     return all_points
+
