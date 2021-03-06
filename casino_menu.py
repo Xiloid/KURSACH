e073c55c04d4e09cc5dec1a98b756e0cc75e3d3e
Xiloid <faustov.roman.a@gmail.com>
2021-03-06 20:13:49 +0200

    commited

--- casino_menu.py
+++ casino_menu.py
@@ -1,8 +1,7 @@
+import json
 from casino_magic import magic
 from casino_blackjack import blackjack
-import colorama
-colorama.init()
-from colorama import Fore, Style
+import ast
 
 
 def main():
@@ -11,30 +10,43 @@ def main():
 
 def menu():
     all_points = 0
-    print(Fore.GREEN + "Главное меню:\
+    rw_file(input('Введите имя игрока: '))
+    print('Главное меню:\
             \n  1. Игра Magic\
             \n  2. Игра Blackjack\
             \n  3. Посмотреть статистику\
             \n  4. Сбросить игровой прогресс\
-            \n  5. Виход", Style.RESET_ALL)
-    print(Fore.BLUE + "\n Выберите действие: ", Style.RESET_ALL)
+            \n  5. Выход')
+    print('\n Выберите действие: ')
     menu = input()
-    if menu == "1":
+    if menu == '1':
         magic(all_points)
         main()
-    elif menu == "2":
+    elif menu == '2':
         blackjack()
         main()
-    elif menu == "3":
+    elif menu == '3':
         pass
-    elif menu == "4":
+    elif menu == '4':
         pass
-    elif menu == "5":
+    elif menu == '5':
         exit('Выходим...')
     else:
-        print(Fore.YELLOW + 'Выберите правильную цифру пункта меню!', Style.RESET_ALL)
+        print('Выберите правильную цифру пункта меню!')
         main()
 
+
+def rw_file(data):
+    with open('data.json', 'w+', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    print('Игрок записан')
+
+
+#def testing():
+#    with open('text.txt') as f:
+#        text = f.read()
+#    d = ast.literal_eval(text)
+#    print(d)
 
 if __name__ == "__main__":
     main()
